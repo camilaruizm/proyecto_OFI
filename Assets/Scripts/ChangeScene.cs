7f6dc245f4212eb45eccd94315b6f69646605756
camilaruizm <camilaruizm@github.com>
2020-06-15 17:34:14 -0500

    video Intro  e interfaz de usuario

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public VideoPlayer VideoPlayer;
+    public string SceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer.loopPointReached += LoadScene;
+    }
+
+    // Update is called once per frame
+    void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
