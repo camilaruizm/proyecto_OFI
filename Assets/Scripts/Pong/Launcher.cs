7c5ec0cefb5b70160e4beb922e0fad7d97dd2f20
MariaBrand <42383415+MariaBrand@users.noreply.github.com>
2020-06-16 18:50:23 -0500

    Scripts Pong

---
+++ Assets/Scripts/Pong/Launcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject proyectilPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         if (Input.GetMouseButtonDown(0)){
+             Debug.Log("Pressed primary button.");
+             GameObject bullet = Instantiate(proyectilPrefab, transform.position, Quaternion.identity) as GameObject;
+             bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+         }
+    }
+}
---
+++ Assets/Scripts/Pong/Walls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Ball")
+        {
+            print("ya");
+            Destroy(collider.gameObject); //Bola
+        }
+
+    }
+}
