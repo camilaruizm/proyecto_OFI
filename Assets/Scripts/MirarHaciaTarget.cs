97ff267874c85fbc1691c4d4b8ce105a9c89db04
camilaruizm <camilaruizm@github.com>
2020-06-19 16:24:33 -0500

    IDK

---
+++ Assets/Scripts/MirarHaciaTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirarHaciaTarget : MonoBehaviour
+{
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Laser");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(this.target.transform.position);
+    }
+}
