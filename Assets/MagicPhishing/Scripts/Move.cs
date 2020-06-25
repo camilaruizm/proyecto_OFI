59c83d264d250bf37b333c6050fdda4f0def45d4
David Ballesteros <david.ballesteros@uao.edu.co>
2020-06-24 22:06:16 -0500

    Movimiento Caña MagicPhishing
    La caña se puede mover con WASD y subir y bajar con las flechas

--- Assets/MagicPhishing/Scripts/Move.cs
+++ Assets/MagicPhishing/Scripts/Move.cs
@@ -6,16 +6,20 @@ public class Move : MonoBehaviour
 {
     public float horizontalMove;
     public float verticalMove;
-    //public float upDownMove;
+    private Vector3 playerInput;
+
     public CharacterController player;
 
-    //public float playerSpeed = 5.0f;
+    public float playerSpeed = 12.0f;
+
+    private Vector3 posInicial = new Vector3(0.0f, -2.6f, 0.0f);
+    private Vector3 pescar = new Vector3(0.0f, -13.33f, 0.0f);
+    public float pescaSpeed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<CharacterController>();
-        //transform.position = new Vector3(3.3f, -2.6f, 3.6f);
     }
 
     // Update is called once per frame
@@ -23,8 +27,18 @@ public class Move : MonoBehaviour
     {
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
-        //upDownMove = Input.GetAxis("Axis 1");
 
-        player.Move(new Vector3(horizontalMove, verticalMove, 0) /* playerSpeed * Time.deltaTime*/);
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        
+        player.Move(playerInput * playerSpeed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pescar, pescaSpeed);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, posInicial, pescaSpeed);
+        }
     }
 }
