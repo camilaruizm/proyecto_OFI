18932efc40a46614f4d301a5e75ea8762c68bc9b
camilaruizm <camilaruizm@github.com>
2020-06-26 07:53:35 -0500

    Revert "Merge branch 'master' of https://github.com/camilaruizm/proyecto_OFI"
    This reverts commit e0228d553a28af605c057f311a25d031cc1833f9, reversing
    changes made to 3341e2c75e08a707bfac011dd929768db8fd36b0.

---
+++ Assets/Scripts/Inicio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inicio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void iniciar()
+    {
+        SceneManager.LoadScene("Nivel 2");
+    }
+
+    public void instrucciones()
+    {
+        SceneManager.LoadScene("Instrucciones");
+    }
+
+    public void regresar()
+    {
+        SceneManager.LoadScene("Inicio");
+    }
+}
